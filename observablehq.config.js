07ed397770f12871e458a418efe9335068a923c7
aviralchawla <avi.champ@hotmail.com>
2026-02-26 19:55:25 -0500

    actions to deploy page

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,6 +3,9 @@ export default {
   // The app’s title; used in the sidebar and webpage titles.
   title: "GSU",
 
+  // GitHub Pages project sites are served at /<repo-name>/.
+  base: "/gsu/",
+
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
@@ -17,7 +20,7 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="logo.png" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
